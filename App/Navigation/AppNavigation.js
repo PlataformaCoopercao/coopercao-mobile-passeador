d82e43a14783873d098fe2025b4cebf06293a945
Rodrigo <ropi2203@gmail.com>
2018-11-29 00:09:51 -0200

    Telas da versão da primeira apresentação

---
+++ App/Navigation/AppNavigation.js
@@ -0,0 +1,28 @@
+import { createStackNavigator, createBottomTabNavigator, createAppContainer } from 'react-navigation'
+import LoginScreen from  '../Containers/LoginScreen.js'
+import PasseadorPasseiosScreen from '../Containers/PasseadorPasseiosScreen.js'
+import PasseiosLivresScreen from '../Containers/PasseiosLivresScreen.js'
+import HistoricoPasseadorScreen from '../Containers/HistoricoPasseadorScreen.js'
+import PasseioScreen from '../Containers/PasseioScreen.js'
+import FeedbackScreen from '../Containers/FeedbackScreen.js'
+
+import styles from './Styles/NavigationStyles'
+
+// Manifest of possible screens
+const PrimaryNav = createBottomTabNavigator({
+  LoginScreen: { screen: LoginScreen },
+  PasseadorPasseiosScreen: { screen: PasseadorPasseiosScreen },
+  PasseiosLivresScreen: { screen: PasseiosLivresScreen},
+  HistoricoPasseadorScreen: { screen: HistoricoPasseadorScreen},
+  PasseioScreen: { screen: PasseioScreen},
+  FeedbackScreen: { screen: FeedbackScreen}
+}, {
+  // Default config for all screens
+  headerMode: 'none',
+  initialRouteName: 'LoginScreen',
+  navigationOptions: {
+    headerStyle: styles.header
+  }
+})
+
+export default PrimaryNav
