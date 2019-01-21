abe82d55fe726052eeadd0748f517f6f779e2f8d
Mateus Fittipaldi <mateusfitti@gmail.com>
2019-01-20 23:44:24 -0200

    Integration on EditarPasseadorScreen

--- App/Containers/EditarPasseadorScreen.js
+++ App/Containers/EditarPasseadorScreen.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Alert } from 'react-native'
 import { ScrollView, KeyboardAvoidingView } from 'react-native'
 import { connect } from 'react-redux'
 import {
@@ -17,13 +18,79 @@ import { Images, Colors } from '../Themes';
 import { TextInput } from 'react-native-gesture-handler';
 import styles from './Styles/EditarPasseadorScreenStyle.js';
 import { Dropdown } from 'react-native-material-dropdown';
+import axios from 'axios';
+import * as firebase from 'firebase';
 
 class  EditarPasseadorScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      uid: firebase.auth().currentUser.uid,
       fontLoading: true, // to load font in expo
+      nome: '',
+      email: '',
+      cpf: '',
+      genero: '',
+      estadoCivil: '',
+      profissao: '',
+      cep: '',
+      bairro: '',
+      rua: '',
+      numero: '',
+      complemento: '',
+      telefone: '',
+      uri: '',
     };
+  }
+
+  getWalkerData () {
+    axios.post('https://us-central1-coopercao-backend.cloudfunctions.net/getWalker', {uid: firebase.auth().currentUser.uid})
+    .then(response => this.setState({nome: response.data.name, email: response.data.email, cpf: response.data.cpf, genero: response.data.gender,
+    estadoCivil: response.data.civilState, profissao: response.data.profession, cep: response.data.address.cep, bairro: response.data.address.area,
+    rua: response.data.address.street, numero: response.data.address.num, complemento: response.data.address.compl, telefone: response.data.phoneNumber,
+    uri: response.data.photoUrl})).catch((error) => {Alert.alert(error.message)});
+  this.update()
+  }
+
+  update () {
+    if(this.state.uri == null || this.state.uri == '') {
+      this.setState({uri:'https://t4.ftcdn.net/jpg/02/15/84/43/240_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg'})
+    }
+    this.forceUpdate()
+  }
+  onSalvarPress = () => {
+    this.submit()
+  }
+
+  submit() {
+    let address = {}
+      address.cep = this.state.cep,
+      address.area = this.state.bairro,
+      address.street = this.state.rua,
+      address.num = this.state.numero,
+      address.compl = this.state.complemento;
+    let walker = {}
+      walker.name = this.state.nome,
+      walker.email = this.state.email,
+      walker.cpf = this.state.cpf,
+      walker.gender = this.state.genero,
+      walker.civilState = this.state.estadoCivil,
+      walker.profession = this.state.profissao,
+      walker.phoneNumber = this.state.telefone,
+      walker.photoUrl = this.state.uri,
+      walker.address = address;
+    let collection = {}
+      collection.uid = this.state.uid,
+      collection.walker = walker;
+    var url = 'https://us-central1-coopercao-backend.cloudfunctions.net/updateWalker';
+    axios.post(url, collection)
+      .then(() => {
+        Alert.alert('Atualizado com sucesso!');
+        this.props.navigation.navigate('PerfilPasseadorScreen');
+      })
+      .catch((error) => {
+        Alert.alert(error.message);
+      });
   }
 
   onValueChange(value: string) {
@@ -33,6 +100,7 @@ class  EditarPasseadorScreen extends Component {
   }
   // required to load native-base font in expo
   async componentWillMount() {
+    this.getWalkerData();
     await Font.loadAsync({
       Roboto: require("native-base/Fonts/Roboto.ttf"),
       Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
@@ -42,7 +110,6 @@ class  EditarPasseadorScreen extends Component {
   }
 
   render() {
-    const uri = "http://www.revistapronews.com.br/repository/news/big-153625870692-edenwololo1.jpg";
     const {navigate} = this.props.navigation;
     if (this.state.fontLoading) {
       return (
@@ -56,62 +123,97 @@ class  EditarPasseadorScreen extends Component {
     } else {
       return (
         <Container style={{ backgroundColor: 'red' }}>
-          <Header style={{ backgroundColor: 'red', marginTop: 15 }}>
+          <Header style={{ backgroundColor: 'red', marginTop: 25 }}>
             <Left>
-              <Icon name='arrow-back' onPress={() => navigate('PerfilPasseadorScreen')} />
+              <Icon name='arrow-back' style={{ marginHorizontal: 10}} onPress={() => navigate('PerfilPasseadorScreen')} />
             </Left>
             <Body>
-              <Title style={{color: Colors.snow}}>{"Perfil"}</Title>
+              <Title style={{ marginHorizontal: 10, color: Colors.snow }}>{strings('EditarPasseadorScreen.edit')}</Title>
             </Body>
             <Right>
-
             </Right>
           </Header>
-          <Content padder style={{ backgroundColor: 'white', alignContent: "stretch" }}>
-            <List>
-              <ListItem style={{ alignSelf: 'center', alignContent: 'center', flexDirection: 'column' }}>
-                <Thumbnail style={{ height: 120, width: 120 }} large source={{ uri: uri }} />
-                <Text style={{color: Colors.steel}}>Alterar Foto</Text>
-              </ListItem>
-              <ListItem style={{ alignSelf: 'center' }}>
 
-                  <Text>{"Alterar Nome:"}</Text>
-                  <Input placeholder={'Eden Wiedemann'} />
-                </ListItem>
-              <ListItem style={{ alignSelf: 'center' }}>
-
-                  <Text>{"Alterar Email:"}</Text>
-                  <Input placeholder={'eden.wiedemann@gmail.com'} />
-                </ListItem>
-              <ListItem style={{ alignSelf: 'center' }}>
-
-                  <Text>{"Alterar Endereço:"}</Text>
-                  <Input placeholder={'Rua dos Bobos, nº 0'} />
-
-              </ListItem>
-              <ListItem style={{ alignSelf: 'center' }}>
-
-                  <Text>{"Alterar Telefone:"}</Text>
-                  <Input placeholder={'91234-5678'} />
-                  
-
-
-              </ListItem>
-              <ListItem style={{ alignSelf: 'center' }}>
-
-                  <Text>{'Confirmar Senha:'}</Text>
-                  <Input secureTextEntry={true} placeholder={'        '} />
-
-
-              </ListItem>
-              
-
-              
-            </List>
-            <Text>{""}</Text>
-            <Button style={styles.botao} onPress={() => navigate('PerfilPasseadorScreen')} >
-              <Text>{"Confirmar"}</Text>
-            </Button>
+          <Content style={{ backgroundColor: 'white', alignContent: "stretch" }}>
+          <Thumbnail style={{ alignSelf:"center" }} large source={{ uri: this.state.uri }} />
+          <List>
+            <ListItem>
+              <InputGroup>
+                <Input placeholder={strings('EditarPasseadorScreen.name')} onChangeText={(text) => { this.setState({ nome: text }) }}>
+                {this.state.nome}</Input>
+              </InputGroup>
+            </ListItem>
+            <ListItem>
+              <InputGroup>
+                <Input placeholder={strings('EditarPasseadorScreen.email')} keyboardType='email-address' autoCapitalize='none' autoCorrect={false}
+                onChangeText={(text) => { this.setState({ email: text }) }} >
+                {this.state.email}</Input>
+              </InputGroup>
+            </ListItem>
+            <ListItem>
+              <InputGroup>
+                <Input placeholder={strings('EditarPasseadorScreen.cpf')} onChangeText={(text) => { this.setState({ cpf: text }) }} >
+                {this.state.cpf}</Input>
+              </InputGroup>
+            </ListItem>
+            <ListItem>
+              <InputGroup>
+                <Input placeholder={strings('EditarPasseadorScreen.gender')} onChangeText={(text) => { this.setState({ genero: text }) }}>
+                {this.state.genero}</Input>
+              </InputGroup>
+            </ListItem>
+            <ListItem>
+              <InputGroup>
+                <Input placeholder={strings('EditarPasseadorScreen.civilState')} onChangeText={(text) => { this.setState({ estadoCivil: text }) }}>
+                {this.state.estadoCivil}</Input>
+              </InputGroup>
+            </ListItem>
+            <ListItem>
+              <InputGroup>
+                <Input placeholder={strings('EditarPasseadorScreen.profession')} onChangeText={(text) => { this.setState({ profissao: text }) }}>
+                {this.state.profissao}</Input>
+              </InputGroup>
+            </ListItem>
+            <ListItem>
+              <InputGroup>
+                <Input placeholder={strings('EditarPasseadorScreen.cep')} onChangeText={(text) => { this.setState({ cep: text }) }}>
+                {this.state.cep}</Input>
+              </InputGroup>
+            </ListItem>
+            <ListItem>
+              <InputGroup>
+                <Input placeholder={strings('EditarPasseadorScreen.area')} onChangeText={(text) => { this.setState({ bairro: text }) }}>
+                {this.state.bairro}</Input>
+              </InputGroup>
+            </ListItem>
+            <ListItem>
+              <InputGroup>
+                <Input placeholder={strings('EditarPasseadorScreen.street')} onChangeText={(text) => { this.setState({ rua: text }) }}>
+                {this.state.rua}</Input>
+              </InputGroup>
+            </ListItem>
+            <ListItem>
+              <InputGroup>
+                <Input placeholder={strings('EditarPasseadorScreen.number')} onChangeText={(text) => { this.setState({ numero: text }) }}>
+                {this.state.numero}</Input>
+              </InputGroup>
+            </ListItem>
+            <ListItem>
+              <InputGroup>
+                <Input placeholder={strings('EditarPasseadorScreen.complement')} onChangeText={(text) => { this.setState({ complemento: text }) }}>
+                {this.state.complemento}</Input>
+              </InputGroup>
+            </ListItem>
+            <ListItem>
+              <InputGroup>
+                <Input placeholder={strings('EditarPasseadorScreen.phone')} onChangeText={(text) => { this.setState({ telefone: text }) }}>
+                {this.state.telefone}</Input>
+              </InputGroup>
+            </ListItem>
+          </List>
+          <Button onPress={this.onSalvarPress} style={{ alignSelf: 'center', marginTop: 20, marginHorizontal: 40, backgroundColor:'red' }}>
+            <Text>{strings('EditarPasseadorScreen.save')}</Text>
+          </Button>
           </Content>
           <Footer style={{ backgroundColor: 'red' }}>
             <FooterTab style={{ backgroundColor: 'red' }}>
